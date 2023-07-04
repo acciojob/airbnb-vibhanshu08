4aca8ba294db611fc25b1001b3ae9d4ac478e595
vibhanshu08 <108286891+vibhanshu08@users.noreply.github.com>
2023-07-04 12:36:39 +0530

    Update HotelManagementController.java

--- src/main/java/com/driver/controllers/HotelManagementController.java
+++ src/main/java/com/driver/controllers/HotelManagementController.java
@@ -21,6 +21,11 @@ import java.util.UUID;
 @RequestMapping("/hotel")
 public class HotelManagementController {
 
+
+@Autowired
+	ServicesSer sr;
+
+	
     @PostMapping("/add-hotel")
     public String addHotel(@RequestBody Hotel hotel){
 
@@ -30,7 +35,7 @@ public class HotelManagementController {
         //in all other cases return SUCCESS after successfully adding the hotel to the hotelDb.
 
 
-        return null;
+        return sr.addHotel(hotel);
     }
 
     @PostMapping("/add-user")
@@ -39,7 +44,7 @@ public class HotelManagementController {
         //You need to add a User Object to the database
         //Assume that user will always be a valid user and return the aadharCardNo of the user
 
-       return null;
+       return sr.addUser(user);
     }
 
     @GetMapping("/get-hotel-with-most-facilities")
@@ -49,7 +54,7 @@ public class HotelManagementController {
         //Incase there is a tie return the lexicographically smaller hotelName
         //Incase there is not even a single hotel with atleast 1 facility return "" (empty string)
 
-        return null;
+        return sr.getHotelWithMostFacilities();
     }
 
     @PostMapping("/book-a-room")
@@ -62,15 +67,69 @@ public class HotelManagementController {
         //If there arent enough rooms available in the hotel that we are trying to book return -1 
         //in other case return total amount paid 
         
-        return 0;
+        return sr.bookARoom(booking);
     }
     
     @GetMapping("/get-bookings-by-a-person/{aadharCard}")
     public int getBookings(@PathVariable("aadharCard")Integer aadharCard)
     {
         //In this function return the bookings done by a person 
-        return 0;
+        return sr.getBookings(aadharCard);
     }
+
+
+
+    // 
+    // @PostMapping("/add-hotel")
+    // public String addHotel(@RequestBody Hotel hotel){
+
+    //     //You need to add an hotel to the database
+    //     //incase the hotelName is null or the hotel Object is null return an empty a FAILURE
+    //     //Incase somebody is trying to add the duplicate hotelName return FAILURE
+    //     //in all other cases return SUCCESS after successfully adding the hotel to the hotelDb.
+
+
+    //     return null;
+    // }
+
+    // @PostMapping("/add-user")
+    // public Integer addUser(@RequestBody User user){
+
+    //     //You need to add a User Object to the database
+    //     //Assume that user will always be a valid user and return the aadharCardNo of the user
+
+    //    return null;
+    // }
+
+    // @GetMapping("/get-hotel-with-most-facilities")
+    // public String getHotelWithMostFacilities(){
+
+    //     //Out of all the hotels we have added so far, we need to find the hotelName with most no of facilities
+    //     //Incase there is a tie return the lexicographically smaller hotelName
+    //     //Incase there is not even a single hotel with atleast 1 facility return "" (empty string)
+
+    //     return null;
+    // }
+
+    // @PostMapping("/book-a-room")
+    // public int bookARoom(@RequestBody Booking booking){
+
+    //     //The booking object coming from postman will have all the attributes except bookingId and amountToBePaid;
+    //     //Have bookingId as a random UUID generated String
+    //     //save the booking Entity and keep the bookingId as a primary key
+    //     //Calculate the total amount paid by the person based on no. of rooms booked and price of the room per night.
+    //     //If there arent enough rooms available in the hotel that we are trying to book return -1 
+    //     //in other case return total amount paid 
+        
+    //     return 0;
+    // }
+    
+    // @GetMapping("/get-bookings-by-a-person/{aadharCard}")
+    // public int getBookings(@PathVariable("aadharCard")Integer aadharCard)
+    // {
+    //     //In this function return the bookings done by a person 
+    //     return 0;
+    // }
 
     @PutMapping("/update-facilities")
     public Hotel updateFacilities(List<Facility> newFacilities,String hotelName){
